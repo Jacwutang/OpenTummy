5f88038129c46005dbd2ce9ea13e0fcbe994b58c
Jack Wu <jackwu350@yahoo.com>
2017-11-21 18:19:24 -0800

    finsih reducers + store

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import configureStore from './store/store.js';
 
 
 document.addEventListener("DOMContentLoaded", () =>{
   const root = document.getElementById('root');
-
+  const store = configureStore();
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
   ReactDOM.render(<h1>Hello World</h1>, root);
 });
---
+++ frontend/reducers/session_reducer.js
@@ -0,0 +1,25 @@
+
+import {RECEIVE_CURRENT_USER} from '../actions/session_actions';
+import merge from 'lodash/merge';
+
+const _nullUser = {
+  currentUser: null
+}
+
+
+//returns POJO
+const sessionReducer = (state = _nullUser, action) => {
+  Object.freeze(state);
+
+  switch(action.type){
+    case RECEIVE_CURRENT_USER:
+      const currentUser = action.currentUser;
+      return merge({}, {currentUser} );
+
+    default:
+      return state;
+  }
+};
+
+
+export default sessionReducer;
