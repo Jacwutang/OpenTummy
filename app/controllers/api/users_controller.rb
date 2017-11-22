1109aeda22c2154aa495e477610681d15290a94e
Jack Wu <jackwu350@yahoo.com>
2017-11-21 17:29:44 -0800

    actions/session-actions.js

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -3,11 +3,36 @@ class Api::UsersController < ApplicationController
   end
 
   def create
+    #if user succesfully created, return json from "api/users/show"
+
+    @user = User.new(user_params)
+
+    if @user.save
+      login(@user)
+      render "api/users/show"
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+
   end
 
   def show
+    @user = User.find(params[:id])
+
+    if @user
+
+    else
+
+    end
+
+
   end
 
   def destroy
+    
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :password)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,8 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resource :session, only: [:create,:destroy]
     resources :users, only:[:create,:show,:destroy]
-
-
   end
+
+  
 
 end
