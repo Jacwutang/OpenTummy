942e8a0eb7dbd677e8df7afbb9a06bf2cd0e5ba3
Jack Wu <jackwu350@yahoo.com>
2017-11-23 11:18:11 -0800

    background image

--- frontend/components/main_page.jsx
+++ frontend/components/main_page.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
 const MainPage = () => (
-  <h1 className = "main-page"> MainPage JOOOOOOOO </h1>
+  <div className = "main-page"> MainPage
+  <form>
+
+  <span>
+    <i className="fa fa-search fa-2x" aria-hidden="true"></i>
+    <input type="text" className = "search-input"
+    name="search"
+    placeholder="Location, Restaurant.."></input>
+
+    <button type="button" className = "search-button"> Find a Table </button>
+  </span>
+
+  </form>
+
+  </div>
 
 )
 
