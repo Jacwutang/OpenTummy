6a9b96dcf9f2ada1cdbc8eccdf4ef0f90390a476
Jack Wu <jackwu350@yahoo.com>
2017-11-23 10:28:41 -0800

    red error messages on session form

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -60,7 +60,7 @@ class SessionForm extends React.Component{
     return(
      <ul>
        {this.props.errors.map((error, i) => (
-         <li key={`error-${i}`}>
+         <li className = "session-form-li" key={`error-${i}`}>
            {error}
          </li>
        ))}
