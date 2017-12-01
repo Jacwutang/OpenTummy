dc6ec4e5ab5ba2e5ddd526cd0cb4d40d906e8d09
Jack Wu <jackwu350@yahoo.com>
2017-12-01 14:56:41 -0800

    minification files fixed typo in createstore

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -3,19 +3,19 @@ import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers/root_reducer';
 
-const configureStore = (preloadedState = {}) => (
+const configureStore = (preloadedState = {}) => {
   if (process.env.NODE_ENV !== 'production') {
   // must use 'require' (import only allowed at top of file)
   const { logger } = require('redux-logger');
   middlewares.push(logger);
-}
+  }
 
-  createStore(
+  return(createStore(
     rootReducer,
     preloadedState,
     applyMiddleware(thunk,logger)
-  )
+  ));
 
-);
+}
 
 export default configureStore;
