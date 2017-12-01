a91a35fefa0c918cf33a085f75a8a59bf5b3d378
Jack Wu <jackwu350@yahoo.com>
2017-12-01 00:39:28 -0800

    Favs show up on profile

--- frontend/components/profile/profile_favorites.jsx
+++ frontend/components/profile/profile_favorites.jsx
@@ -22,21 +22,21 @@ class ProfileFavorites extends React.Component{
   }
 
   componentWillMount(){
-    var rest_key_arr = [];
-
-    for(let x = 0; x < this.props.my_favorites.length ;x++){
-      if(rest_key_arr.includes(this.props.my_favorites[x].restaurant_id) !== true){
-        rest_key_arr.push(this.props.my_favorites[x].restaurant_id);
-      }
-    }
-    console.log(rest_key_arr);
-
-    var my_favorite_restaurants = [];
-
-    for(let x = 0; x < rest_key_arr.length; x++){
-      my_favorite_restaurants.push(this.props.restaurantsIndex[rest_key_arr[x]]);
-    }
-    console.log(my_favorite_restaurants);
+    // var rest_key_arr = [];
+    //
+    // for(let x = 0; x < this.props.my_favorites.length ;x++){
+    //   if(rest_key_arr.includes(this.props.my_favorites[x].restaurant_id) !== true){
+    //     rest_key_arr.push(this.props.my_favorites[x].restaurant_id);
+    //   }
+    // }
+    // console.log(rest_key_arr);
+    //
+    // var my_favorite_restaurants = [];
+    //
+    // for(let x = 0; x < rest_key_arr.length; x++){
+    //   my_favorite_restaurants.push(this.props.restaurantsIndex[rest_key_arr[x]]);
+    // }
+    // console.log(my_favorite_restaurants);
 
   }
 
@@ -45,13 +45,66 @@ class ProfileFavorites extends React.Component{
     // if(Object.keys(my_restaurants).length === 0){
     //   return null;
     // }
+    var rest_key_arr = [];
+
+    for(let x = 0; x < this.props.my_favorites.length ;x++){
+      if(rest_key_arr.includes(this.props.my_favorites[x].restaurant_id) !== true){
+        rest_key_arr.push(this.props.my_favorites[x].restaurant_id);
+      }
+    }
+
+
+    var my_favorite_restaurants = [];
+
+    for(let x = 0; x < rest_key_arr.length; x++){
+      my_favorite_restaurants.push(this.props.restaurantsIndex[rest_key_arr[x]]);
+    }
+
 
     return(
-      <h1> Hi </h1>
+      <div>
+
+        <ul className = "profile-restaurants-index-ul">
+          {my_favorite_restaurants.map( (rest) => (
+
+            <li className = "profile-restaurants-index-li"
+            key = {rest.id}>
+
+                    <div className = "profile-restaurants-index-div-left">
+
+                      <Link to={`/restaurants/${rest.id}`}>
+                        <img className = "thumbnail" src={rest.thumbnail}/>
+                      </Link>
+
+                        <h2> {rest.name} </h2>
+                        <h2> {rest.address} </h2>
+                        <h2> {rest.city} , {rest.state}</h2>
+
+                    </div>
+
+                    <div className = "profile-restaurants-index-div-right">
+                      <button type="button" onClick={this.handleSubmit(rest.id)}> Delete </button>
+                      <br/>
+                      <br/>
+
+                    </div>
+
+
+            </li>
+
+          ))}
+
+        </ul>
+
+
+      </div>
     );
 
 
-}
+
+
+
+  }
 }
 
 // const {my_restaurants} = this.props;
