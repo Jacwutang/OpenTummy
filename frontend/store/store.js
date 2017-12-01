abe67bd64eea23d88ca411ef48b98e7493d1ba11
Jack Wu <jackwu350@yahoo.com>
2017-12-01 15:32:34 -0800

    redux logger

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,7 +1,8 @@
 import {createStore, applyMiddleware} from 'redux';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers/root_reducer';
+
 const middlewares = [thunk];
 if (process.env.NODE_ENV !== 'production') {
 // must use 'require' (import only allowed at top of file)
