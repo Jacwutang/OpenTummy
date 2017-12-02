cd4f5c0f10f0c32111d9c3adec5086d840eed9cd
Jack Wu <jackwu350@yahoo.com>
2017-12-02 15:40:53 -0800

    front-page css

--- frontend/components/main_page.jsx
+++ frontend/components/main_page.jsx
@@ -4,44 +4,55 @@ const MainPage = () => (
 
 
   <div className = "main-page">
-    
+    <div className = "featured-areas">
+      <h1 className = "main-page-h1"> Explore Restaurants </h1>
+      <hr/>
 
-    <div className= "main-page-reservation">
+      <div className = "featured-areas-row1">
+        <div className = "col-1-3">
 
+          <img className = "featured-areas-img" src="http://travelhdwallpapers.com/wp-content/uploads/2014/04/Statue-Of-Liberty-24.jpg"/>
+          <div className = "center">
+            New York City
+          </div>
 
+        </div>
 
-    <select className = "main-page-reservation-select">
-        <option value="" disabled >
-        1 People</option>
+        <div className="col-1-3">
+          <img className = "featured-areas-img" src="https://upload.wikimedia.org/wikipedia/commons/2/26/Chicago_Theatre_blend.jpg"/>
 
+          <div className = "center">
+            Chicago
+          </div>
 
-        <option value={1}> 1 people</option>
-        <option value={2}> 2 people</option>
-        <option value={3}> 3 people</option>
-        <option value={4}> 4 people</option>
-        <option value={5}> 5 people</option>
-
-      </select>
-
-
-      <select className = "main-page-reservation-select">
-          <option value="" disabled>
-            7:00 PM </option>
-
-          <option value="7:00 PM" type ="text">7:00 PM</option>
-
-      </select>
+        </div>
 
 
-    <input type="text" className = "search-input"
-      name="search"
-      placeholder="Location, Restaurant..">
-    </input>
+        <div className="col-1-3">
+          <img className = "featured-areas-img" src="http://dobsonchronicles.com/wp-content/uploads/2016/08/SF.jpg"/>
 
-      <button type="button" className = "search-button"> Find a Table </button>
+          <div className = "center">
+            San Francisco
+          </div>
 
+        </div>
+
+      </div>
+
+
+
+
+
+
+
+
+      <div className = "-featured-areas-row2">
+
+
+      </div>
 
     </div>
+
 
 
   </div>
@@ -65,3 +76,46 @@ export default MainPage;
 // </span>
 //
 // </form>
+
+
+
+
+
+
+
+// <div className= "main-page-reservation">
+//
+//
+//
+// <select className = "main-page-reservation-select">
+//     <option value="" disabled >
+//     1 People</option>
+//
+//
+//     <option value={1}> 1 people</option>
+//     <option value={2}> 2 people</option>
+//     <option value={3}> 3 people</option>
+//     <option value={4}> 4 people</option>
+//     <option value={5}> 5 people</option>
+//
+//   </select>
+//
+//
+//   <select className = "main-page-reservation-select">
+//       <option value="" disabled>
+//         7:00 PM </option>
+//
+//       <option value="7:00 PM" type ="text">7:00 PM</option>
+//
+//   </select>
+//
+//
+// <input type="text" className = "search-input"
+//   name="search"
+//   placeholder="Location, Restaurant..">
+// </input>
+//
+//   <button type="button" className = "search-button"> Find a Table </button>
+//
+//
+// </div>
