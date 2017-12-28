fb8c79fe2fa4afc6203d383b539124fbfdd5f80c
Jack Wu <jackwu350@yahoo.com>
2017-12-27 23:06:02 -0800

    implemented Restaurant Index li  3 column display

--- frontend/components/restaurant/restaurant_index_item.jsx
+++ frontend/components/restaurant/restaurant_index_item.jsx
@@ -9,11 +9,6 @@ class RestaurantIndexItem extends React.Component{
     super(props);
 
   }
-
-
-
-
-
   // const category = (rest.category === null) ? "No Category" : rest.category;
 
   samplePrice(){
@@ -25,7 +20,7 @@ class RestaurantIndexItem extends React.Component{
 
   render(){
     const {rest} = this.props;
-    // console.log(rest);
+
 
     const category = (rest.category === null) ? "No Category" : rest.category;
 
@@ -37,29 +32,30 @@ class RestaurantIndexItem extends React.Component{
             <img className = "thumbnail" src={rest.thumbnail}/>
           </Link>
 
+            <div className = "restaurant-index-left-div-rest">
               <h2> {rest.name} </h2>
               <h3 className = "h3-restaurant-index-item-price"> {this.samplePrice()} </h3>
 
 
             <br/>
-            <h3 className = "h3-restaurant-index-item"> Average Rating *** </h3>
+            <h3> Average Rating *** </h3>
             <br/>
-              <h3 className = "h3-restaurant-index-item"> {category} | {rest.city} </h3>
+              <h3> {category} | {rest.city} </h3>
             <br/>
+
 
           <i className="fa fa-fire fa-2x" aria-hidden="true"></i>  Booked 25 times today!
-
           </div>
 
           <div className = "restaurant-index-right-div">
-            <h3 className ="review-h3"> Dope. Would come again 10/10. </h3>
-            <br/>
-            <br/>
-            <h4 className = "user-h4"> -Username </h4>
-
+            $$$$
+            Chicago
+          </div>
 
 
           </div>
+
+
 
 
 
@@ -72,3 +68,8 @@ class RestaurantIndexItem extends React.Component{
 };
 
 export default RestaurantIndexItem;
+
+// <div className = "restaurant-index-right-div">
+//   $$$$
+//   Chicago
+// </div>
