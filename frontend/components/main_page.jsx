104f2aec52599c5a0a902190d52805791f8700ad
Jack Wu <jackwu350@yahoo.com>
2017-12-28 22:25:08 -0800

    featured areas images overlay with text

--- frontend/components/main_page.jsx
+++ frontend/components/main_page.jsx
@@ -21,14 +21,23 @@ class MainPage extends React.Component{
           <div className = "featured-areas">
             <div className = "media">
               <img className = "featured-areas-img" src="http://travelhdwallpapers.com/wp-content/uploads/2014/04/Statue-Of-Liberty-24.jpg"/>
+              <div className = "center">
+                New York City
+              </div>
             </div>
 
             <div className = "media">
               <img className = "featured-areas-img" src="https://upload.wikimedia.org/wikipedia/commons/2/26/Chicago_Theatre_blend.jpg"/>
+              <div className = "center">
+                Chicago
+              </div>
             </div>
 
             <div className = "media">
                <img className = "featured-areas-img" src="http://dobsonchronicles.com/wp-content/uploads/2016/08/SF.jpg"/>
+               <div className = "center">
+                 San Francisco
+               </div>
             </div>
           </div>
 
