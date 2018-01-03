717dd58e6c6162cff5a906af9ca4ef471e52ef02
Jack Wu <jackwu350@yahoo.com>
2018-01-02 17:13:54 -0800

    edit Reviews schema. add ratings

--- db/migrate/20180102234313_create_reviews.rb
+++ db/migrate/20180102234313_create_reviews.rb
@@ -3,6 +3,7 @@ class CreateReviews < ActiveRecord::Migration[5.1]
     create_table :reviews do |t|
       t.integer :user_id, null:false
       t.integer :restaurant_id, null:false
+      t.integer :rating, null:false
       t.text :body, null:false
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20180102234313) do
   create_table "reviews", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "restaurant_id", null: false
+    t.integer "rating", null: false
     t.text "body", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
